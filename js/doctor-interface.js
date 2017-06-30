e551ff93391c31668f3bf341c338810c994a1e0f
Marilyn Carlin <marilyn.s.carlin@gmail.com>
2017-06-30 13:32:36 -0700

    add functionality to provide users with list of insurance providers; currently not working.

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,17 +1,24 @@
 var User = require('./../js/user.js').userModule;
 var Doctor = require('./../js/doctor.js').doctorModule;
+var Insurance = require('./../js/insurance.js').insuranceModule;
+
+
+var insuranceSelection = function(array) {
+  array.forEach(function(data) {
+    $("#insurance").append('<option value="'+ data.id +'">'+ data.name +'</option>');
+  });
+};
 
 
 $(document).ready(function() {
+  var currentUserObject = new User();
+  currentUserObject.getInsurances();
 
   $("#user-form").submit(function(event) {
     event.preventDefault();
     $(".showDoctors").empty();
-
-    var currentUserObject = new User();
     var location = $("#location").val();
     var symptom = $("#symptom").val();
-
     currentUserObject.getDoctors(symptom, 'united healthcare');
   });
 });
---
+++ js/insurance.js
@@ -0,0 +1,22 @@
+var apiKey = require('./../.env').apiKey;
+var User = require('./../js/user.js').userModule;
+
+function Insurance(id, name) {
+  this.id = id;
+  this.name = name;
+}
+
+var insuranceArray = [];
+
+User.prototype.getInsurances = function() {
+  $.get('https://api.betterdoctor.com/2016-03-01/insurances?user_key=' + apiKey)
+    .then(function(response) {
+      response.data.forEach(function(element) {
+        var newInsurance = new Insurance(element.uid, element.name);
+        insuranceArray.push(newInsurance);
+      });
+      insuranceSelection(insuranceArray);
+    });
+  };
+
+exports.insuranceModule = Insurance;
