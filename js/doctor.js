ad1c85f05ce27279386ce23a4bd777635780f935
Marilyn Carlin <marilyn.s.carlin@gmail.com>
2017-07-05 08:26:33 -0700

    fix typo in doctor.js

--- js/doctor.js
+++ js/doctor.js
@@ -6,7 +6,7 @@ function User() {
 var doctorArray = [];
 
 User.prototype.getDoctors = function(symptom, range) {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + symptom + '&location=45.5231%2C-122.6765%2C%' + range + '&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + symptom + '&location=45.5231%2C-122.6765%2C' + range + '&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
     .then(function(response) {
       response.data.forEach(function(element) {
        doctorArray.push( {
